f9a20ac014fe6e9e6041b81b15974702ba251e57
marco <marcovazquez77@hotmail.com>
2018-05-10 20:34:36 -0500

    first commit

---
+++ app/src/main/java/com/darkovr/patmimplicitintentions/MainActivity.java
@@ -0,0 +1,20 @@
+package com.darkovr.patmimplicitintentions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.darkovr.patmimplicitintentions.Fragments.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container_main, new  MainFragment())
+                .commit();
+    }
+}
